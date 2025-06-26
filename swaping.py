b09a4dfc911e9d38634c5b612ed4f27211c4978e
ISHIKA <ishikaudvanshi@gmail.com>
2025-06-26 02:00:51 -0400

    Add files via upload
    swapping the number by two different method

---
+++ swaping.py
@@ -0,0 +1,18 @@
+# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# temp=a
+# a=b
+# b=temp
+# print("a=",a)
+# print("b=",b)
+# other method of swapping
+# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# a=a+b
+# b=a-b
+# a=a-b
+# print("a=",a)
+# print("b=",b)
+a,b='2',3
+text="@"
+print((a+text)*b)
