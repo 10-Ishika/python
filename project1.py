3b0023aa03a3bb7668b5939163c6a189e0edce2e
ISHIKA <ishikaudvanshi@gmail.com>
2025-06-30 10:28:54 -0400

    rock , paper, scissor game

--- project1.py
+++ project1.py
@@ -5,20 +5,21 @@
 # 3.paper wins against rock
 
 import random
+print("!!!! WELCOME TO THE ROCK , PAPER , SCISSOR GAME !!!!")
 rock=0
 paper=1
 scissor=2
 user=int(input("Enter 0 for rock , 1 for paper , 2 for scissor:"))
-print("user is =",user)
-comp=random.randint(0,2)
-print("comp is =",comp)
-if user == comp:
+print("user throw =",user)
+computer=random.randint(0,2)
+print("computer is =",computer)
+if user == computer:
     print("game is tie")
-elif user>comp:
-    print("user win")
-elif user<comp:
-    print("comp")
-elif user==0 and comp==2:
-    print("user win")
-elif user==2 and comp==0:
-    print("comp win")
+elif user>computer:
+    print("user wins the game!!")
+elif user<computer:
+    print("comp wins the game!!")
+elif user==0 and computer==2:
+    print("user wins the game!!")
+elif user==2 and computer==0:
+    print("computer wins the game!!")
